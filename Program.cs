79df4b77666e41beb42f0c14fb4d10be3a3e10d9
rhofman <hoffroma24@gmail.com>
2020-03-30 10:15:34 +0300

    task complete

--- Program.cs
+++ Program.cs
@@ -9,22 +9,60 @@ namespace HomeworkTemplate
         {
             Func<Task3, string> TaskSolver = task =>
             {
-                // Your solution goes here
-                // You can get all needed inputs from task.[Property]
-                // Good luck!
-                string UserLongitude = task.UserLongitude;
-                string UserLatitude = task.UserLatitude;
-                int placesAmount = task.DefibliratorStorages.Length;
 
-                for (int i = 0; i < placesAmount; i++)
+                string strUserLongitude = task.UserLongitude;
+                float userLongitude = LoadFloatFromString(strUserLongitude);
+
+                string strUserLatitude = task.UserLatitude;
+                float userLatitude = LoadFloatFromString(strUserLatitude);
+
+                float minDistance = float.MaxValue;
+                int minDistanceIndex = -1;
+
+                int placesCount = task.DefibliratorStorages.Length;
+                for (int i = 0; i < placesCount; i++)
                 {
-                    string defibliratorStorage = task.DefibliratorStorages[i];
-                }   
-                
-                return "My answer";
+                    string defibrillatorStorage = task.DefibliratorStorages[i];
+                    string[] storageData = defibrillatorStorage.Split(';');
+
+                    string strLongitude = storageData[2];
+                    float longitude = LoadFloatFromString(strLongitude);
+
+                    string strLatitude = storageData[3];
+                    float latitude = LoadFloatFromString(strLatitude);
+
+                    float d = CalculateDistance(longitude, latitude, userLongitude, userLatitude);
+
+                    if (d < minDistance)
+                    {
+                        minDistance = d;
+                        minDistanceIndex = i;
+                    }
+                }
+
+                string[] closestStorageData = task.DefibliratorStorages[minDistanceIndex].Split(';');
+                string name = closestStorageData[0];
+                string address = closestStorageData[1];
+                return $"Name: {name}; Address: {address}";
             };
 
             Task3.CheckSolver(TaskSolver);
+        }
+        
+        static float LoadFloatFromString(string str)
+        {
+            str.Replace(',', '.');
+            return float.Parse(str);
+        }
+
+        static float CalculateDistance(float longitudeA, float latitudeA, float longitudeB, float latitudeB)
+        {
+            const float EARTH_RADIUS = 6371;
+
+            float x = (longitudeB - longitudeA) * MathF.Cos((latitudeA + latitudeB) / 2f);
+            float y = (latitudeB - latitudeA);
+
+            return MathF.Sqrt((x * x) + (y * y)) * EARTH_RADIUS;
         }
     }
 }
